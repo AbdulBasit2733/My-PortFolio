eafd979c925341a288b78afe0d84dab0e8f03627
Abdul Basit Khan <99525295+AbdulBasit2733@users.noreply.github.com>
2023-10-02 11:14:38 +0530

    using yaml to deploy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/My-PortFolio/",
-});
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/My-PortFolio/'
+  }
+
+  return config
+})
