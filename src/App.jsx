e75e8b357c784e6702d383f8f99d97b5c08af209
Abdul Basit Khan <99525295+AbdulBasit2733@users.noreply.github.com>
2023-10-02 13:41:20 +0530

    using yaml to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,13 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import About from "./Components/About";
 import Education from "./Components/Education";
 import Contact from "./Components/Contact";
-import PageNotFound from "./Components/PageNotFound";
 const App = () => {
   return (
     <>
       <BrowserRouter basename="/My-PortFolio/">
         <Navbar />
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contactme" element={<Contact />} />
