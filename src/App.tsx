6ad984363bc88a721f6eb68462d452be5ab6750c
Abdul Basit <abdulbasitkhan2733@gmail.com>
2025-01-03 23:41:08 +0530

    Redesigning The Website

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { AppSiderbar } from './components/AppSidebar'
+
+const App = () => {
+  return (
+    <main>
+      <AppSiderbar />
+    </main>
+  )
+}
+
+export default App
